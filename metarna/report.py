68165b0d2e5079fa5e43971ef2cd456f6fd0ddf2
Prashant Sinha <prashant@ducic.ac.in>
2016-05-04 18:39:40 +0530

    docs

--- metarna/report.py
+++ metarna/report.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # metaRNA
-'''Target Scan Report Generation'''
+'''Target Scan Report Generation
+
+Each hits contain the following three attributes:
+ - aln_mirna
+ - aln_map
+ - aln_utr
+Where aln_utr is the sequence from the cDNA, aln_map are the base pairs, and
+aln_mirna is the miRNA sequence.
+
+Expected Render:
+  acgguaacucaagguuuUAGCGGu
+                   ||:|:|
+  -------acaagatgccATTGTCc
+'''
 
